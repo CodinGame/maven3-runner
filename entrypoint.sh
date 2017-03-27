c194fa60ec79f4df3a0fe352ca7d5d22ac3ca196
Sylvain Fraïssé <sylvain.fraisse@codingame.com>
2017-03-27 11:16:14 +0200

    refact: move the maven errors in a specific channel

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo 'Compile and run your code'
+echo 'CG> redirect-streams --pattern "\[ERROR\].*" "maven error"'
 MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1" mvn --quiet test -DfailIfNoTests=false -Dtest=$*
